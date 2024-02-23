c7fa77321be9a5d93076276ca9c7a4509add8816
meggi <mblam@buffalo.edu>
2024-02-23 16:38:48 -0500

    "LO1 Progress"

--- util/request.py
+++ util/request.py
@@ -6,6 +6,9 @@ class Request:
         decoding = request.decode()
         splitting = decoding.split('\r\n')
         split_one = splitting[0].split(' ')
+        body = splitting[-1]
+        # print(body)
+        # print(splitting)
 
         self.body = b""
         self.method = split_one[0]
@@ -17,11 +20,10 @@ class Request:
         self.cookies = {}
 
         for elem in splitting:
-            if "ookie" not in elem:
-                mini_split = elem.split(' ')
-                if mini_split[0].endswith(":"):
-                    self.headers[mini_split[0].rstrip(":")] = mini_split[1]
-            else:
+            if ("ookie" not in elem) and (":" in elem):
+                mini_split = elem.split(': ')
+                self.headers[mini_split[0]] = mini_split[1]
+            elif "ookie" in elem:
                 self.headers[elem[:elem.index(":")]] = elem.split(": ")[1]
 
         for key in self.headers:
@@ -32,7 +34,7 @@ class Request:
                     self.cookies[mini_split[0]] = mini_split[1]
 
         if "Content-Length" in self.headers:
-            self.body += splitting[-1].encode()
+            self.body += body.encode()
 
 
 def test1():
@@ -64,8 +66,15 @@ def test3():
     assert "Content-Type" in request.headers
     assert request.body == b"hello"
 
+def test4():
+    #this test is only for testing with print statements
+    request = Request(b'GET /~mhertz/2024spring/cse442/stylesheet.php?page=tables HTTP/1.1\r\nAccept: text/css,*/*;q=0.1\r\nAccept-Encoding: gzip, deflate, br, zstd\r\nAccept-Language: en-US,en;q=0.9\r\nConnection: keep-alive\r\nCookie: nmstat=20b3e6e3-c053-c0f8-3547-f23cf4757e9f; _ga_CB14SJGJBF=GS1.1.1694821723.8.1.1694822678.0.0.0; _ga_J7VSG6CS88=GS1.1.1695341007.1.1.1695341036.0.0.0\r\nHost: cse.buffalo.edu\r\nPragma: no-cache\r\nReferer: https://cse.buffalo.edu/~mhertz/2024spring/cse442/\r\nSec-Fetch-Dest: style\r\nUser-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari')
+    # for elem in request.headers:
+    #     print(elem)
+    #     print(request.headers[elem])
 
 if __name__ == '__main__':
     test1()
     test2()
     test3()
+    test4()
